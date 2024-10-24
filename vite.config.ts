b5827961d79312b3759c2fef7c51a92987f31e99
at4 <at4@jc4.so-net.ne.jp>
2024-10-24 23:26:28 +0200

    Add some packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      }
+    }
+  },
   plugins: [react()],
-})
+});
