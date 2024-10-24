9d2787152e0aaceff116f87e6dc035de02f63633
at4 <at4@jc4.so-net.ne.jp>
2024-10-24 23:37:04 +0200

    Update App importing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import viteLogo from "/vite.svg";
 import reactLogo from "./assets/react.svg";
 import "./App.scss";
 
-function App() {
+export const App = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -30,6 +30,4 @@ function App() {
       </p>
     </>
   );
-}
-
-export default App;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
-import App from "./App.tsx";
+import { App } from "./App";
 
 // biome-ignore lint: no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
